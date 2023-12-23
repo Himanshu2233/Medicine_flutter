1fc49beb551d4f05ddca7033b712e21c42480209
Himanshu2233 <sahkishor24@gmail.com>
2023-12-23 12:50:20 +0545

    added the sytem to add or delete reminder

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,17 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:timezone/timezone.dart' as tz;
 
 void main() {
-  runApp(MedicineReminderApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => MedicineProvider()),
+      Provider(create: (context) => NotificationService()),
+    ],
+    child: MedicineReminderApp(),
+  ));
 }
 
 class MedicineReminderApp extends StatelessWidget {
@@ -16,143 +24,16 @@ class MedicineReminderApp extends StatelessWidget {
       title: 'Medicine Reminder',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        colorScheme: ThemeData().colorScheme.copyWith(
+              secondary: Colors.orangeAccent,
+            ),
       ),
       home: HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-  List<Medicine> medicines = [];
-
-  @override
-  void initState() {
-    super.initState();
-    initializeNotifications();
-    loadMedicines();
-  }
-
-  Future<void> initializeNotifications() async {
-    final AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('@mipmap/ic_launcher');
-
-    final InitializationSettings initializationSettings =
-        InitializationSettings(
-            android: initializationSettingsAndroid, iOS: null, macOS: null);
-
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
-        onSelectNotification: selectNotification);
-  }
-
-  Future<void> _showNotification(String medicineName) async {
-    const AndroidNotificationDetails androidPlatformChannelSpecifics =
-        AndroidNotificationDetails(
-      'channel_id',
-      'channel_name',
-      importance: Importance.max,
-      priority: Priority.high,
-      playSound: true,
-      enableVibration: true,
-    );
-
-    const NotificationDetails platformChannelSpecifics =
-        NotificationDetails(android: androidPlatformChannelSpecifics);
-
-    await flutterLocalNotificationsPlugin.show(
-      0,
-      'Medicine Reminder',
-      'It\'s time to take $medicineName!',
-      platformChannelSpecifics,
-      payload: 'Default_Sound',
-    );
-  }
-
-  Future<void> selectNotification(String? payload) async {
-    // Handle notification click event
-  }
-
-  Future<void> loadMedicines() async {
-    try {
-      final directory = await getApplicationDocumentsDirectory();
-      final file = File('${directory.path}/medicines.json');
-
-      if (file.existsSync()) {
-        final content = await file.readAsString();
-        final List<dynamic> decoded = jsonDecode(content);
-
-        setState(() {
-          medicines = decoded.map((json) => Medicine.fromJson(json)).toList();
-        });
-      }
-    } catch (e) {
-      print('Error loading medicines: $e');
-    }
-  }
-
-  Future<void> saveMedicines() async {
-    try {
-      final directory = await getApplicationDocumentsDirectory();
-      final file = File('${directory.path}/medicines.json');
-
-      final encoded = jsonEncode(medicines);
-      await file.writeAsString(encoded);
-    } catch (e) {
-      print('Error saving medicines: $e');
-    }
-  }
-
-  void addMedicine(String name, TimeOfDay time) {
-    final medicine = Medicine(name: name, time: time);
-
-    setState(() {
-      medicines.add(medicine);
-    });
-
-    saveMedicines();
-    scheduleNotification(medicine);
-  }
-
-  Future<void> scheduleNotification(Medicine medicine) async {
-    final now = DateTime.now();
-    final scheduledTime = DateTime(
-        now.year, now.month, now.day, medicine.time.hour, medicine.time.minute);
-
-    if (scheduledTime.isBefore(now)) {
-      scheduledTime.add(Duration(days: 1));
-    }
-
-    final timeDifference = scheduledTime.difference(now);
-    final secondsUntilNotification = timeDifference.inSeconds;
-
-    var tz;
-    await flutterLocalNotificationsPlugin.zonedSchedule(
-      0,
-      'Medicine Reminder',
-      'It\'s time to take ${medicine.name}!',
-      tz.TZDateTime.from(scheduledTime, tz.local),
-      const NotificationDetails(
-        android: AndroidNotificationDetails(
-          'channel_id',
-          'channel_name',
-          importance: Importance.max,
-          priority: Priority.high,
-          playSound: true,
-          enableVibration: true,
-        ),
-      ),
-      androidAllowWhileIdle: true,
-      uiLocalNotificationDateInterpretation:
-          UILocalNotificationDateInterpretation.absoluteTime,
-    );
-  }
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -163,29 +44,73 @@ class _HomeScreenState extends State<HomeScreen> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
-            child: ListView.builder(
-              itemCount: medicines.length,
-              itemBuilder: (context, index) {
-                final medicine = medicines[index];
-                return ListTile(
-                  title: Text(medicine.name),
-                  subtitle: Text(
-                      'Time: ${medicine.time.hour}:${medicine.time.minute}'),
-                );
-              },
-            ),
+            child: MedicineList(),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                showAddMedicineDialog(context);
-              },
-              child: Text('Add Medicine'),
-            ),
+            child: AddMedicineButton(),
           ),
         ],
       ),
+    );
+  }
+}
+
+class MedicineList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MedicineProvider>(
+      builder: (context, provider, child) {
+        return ListView.builder(
+          itemCount: provider.medicines.length,
+          itemBuilder: (context, index) {
+            final medicine = provider.medicines[index];
+            return MedicineCard(medicine: medicine);
+          },
+        );
+      },
+    );
+  }
+}
+
+class MedicineCard extends StatelessWidget {
+  final Medicine medicine;
+
+  MedicineCard({required this.medicine});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8.0),
+      child: ListTile(
+        title: Text(
+          medicine.name,
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text(
+          'Time: ${TimeOfDay(hour: medicine.time.hour, minute: medicine.time.minute).format(context)}',
+          style: TextStyle(fontSize: 16),
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.delete),
+          onPressed: () {
+            context.read<MedicineProvider>().deleteMedicine(medicine);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class AddMedicineButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      onPressed: () {
+        showAddMedicineDialog(context);
+      },
+      icon: Icon(Icons.add),
+      label: Text('Add Medicine'),
     );
   }
 
@@ -199,12 +124,16 @@ class _HomeScreenState extends State<HomeScreen> {
         return AlertDialog(
           title: Text('Add Medicine'),
           content: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
               TextField(
                 onChanged: (value) {
                   medicineName = value;
                 },
-                decoration: InputDecoration(labelText: 'Medicine Name'),
+                decoration: InputDecoration(
+                  labelText: 'Medicine Name',
+                  border: OutlineInputBorder(),
+                ),
               ),
               SizedBox(height: 16),
               ElevatedButton(
@@ -228,7 +157,9 @@ class _HomeScreenState extends State<HomeScreen> {
             ElevatedButton(
               onPressed: () {
                 if (medicineName.isNotEmpty) {
-                  addMedicine(medicineName, selectedTime!);
+                  context
+                      .read<MedicineProvider>()
+                      .addMedicine(medicineName, selectedTime!);
                 }
                 Navigator.of(context).pop();
               },
@@ -238,6 +169,104 @@ class _HomeScreenState extends State<HomeScreen> {
         );
       },
     );
+  }
+}
+
+class MedicineProvider with ChangeNotifier {
+  List<Medicine> _medicines = [];
+
+  List<Medicine> get medicines => _medicines;
+
+  void addMedicine(String name, TimeOfDay time) {
+    final medicine = Medicine(name: name, time: time);
+    _medicines.add(medicine);
+    _scheduleNotification(medicine);
+    _saveMedicines();
+    notifyListeners();
+  }
+
+  void deleteMedicine(Medicine medicine) {
+    _medicines.remove(medicine);
+    _cancelNotification(medicine);
+    _saveMedicines();
+    notifyListeners();
+  }
+
+  Future<void> _saveMedicines() async {
+    try {
+      final directory = await getApplicationDocumentsDirectory();
+      final file = File('${directory.path}/medicines.json');
+      final encoded = jsonEncode(_medicines);
+      await file.writeAsString(encoded);
+    } catch (e) {
+      print('Error saving medicines: $e');
+    }
+  }
+
+  Future<void> _scheduleNotification(Medicine medicine) async {
+    final notificationService = NotificationService();
+    await notificationService.scheduleNotification(medicine);
+  }
+
+  Future<void> _cancelNotification(Medicine medicine) async {
+    final notificationService = NotificationService();
+    await notificationService.cancelNotification(medicine);
+  }
+}
+
+class NotificationService {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  Future<void> initializeNotifications() async {
+    final AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+            android: initializationSettingsAndroid, iOS: null, macOS: null);
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: (payload) async {
+      // Handle notification click event
+    });
+  }
+
+  Future<void> scheduleNotification(Medicine medicine) async {
+    final now = DateTime.now();
+    final scheduledTime = DateTime(
+        now.year, now.month, now.day, medicine.time.hour, medicine.time.minute);
+
+    if (scheduledTime.isBefore(now)) {
+      scheduledTime.add(Duration(days: 1));
+    }
+
+    final timeDifference = scheduledTime.difference(now);
+    final secondsUntilNotification = timeDifference.inSeconds;
+
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      medicine.hashCode,
+      'Medicine Reminder',
+      'It\'s time to take ${medicine.name}!',
+      tz.TZDateTime.from(scheduledTime, tz.local),
+      const NotificationDetails(
+        android: AndroidNotificationDetails(
+          'channel_id',
+          'channel_name',
+          importance: Importance.max,
+          priority: Priority.high,
+          playSound: true,
+          enableVibration: true,
+        ),
+      ),
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+    );
+  }
+
+  Future<void> cancelNotification(Medicine medicine) async {
+    await flutterLocalNotificationsPlugin.cancel(medicine.hashCode);
   }
 }
 
